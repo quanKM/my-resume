093b94d16c2b4bec834098affb072dcb1884a837
QuanKM <quankhongminh109@gmail.com>
2021-02-11 18:00:21 +0700

    update: experience section

--- src/components/Home/Resume/index.tsx
+++ src/components/Home/Resume/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useEffect } from 'react'
-import './index.scss'
 import { Element } from 'react-scroll'
+import './index.scss'
 
 const Resume: FC = () => {
   useEffect(() => {
@@ -11,97 +11,108 @@ const Resume: FC = () => {
   }, [])
 
   return (
-    <Element className="resume font-mont bg-primary" id="resume" name="resume">
-      <div className="container mx-auto py-32 px-4 sm:px-0">
-        <div className="section-title">
-          <h3 className="text-blue-500 ">Experience & Education</h3>
+    <Element className='resume font-mont bg-primary' id='resume' name='resume'>
+      <div className='container mx-auto py-32 px-4 sm:px-0'>
+        <div className='section-title'>
+          <h3 className='text-blue-500 '>Experience & Education</h3>
           <h2>My Experience</h2>
         </div>
-        <ul className="timeline timeline-centered mt-4">
+        <ul className='timeline timeline-centered mt-4'>
           <li
-            className="timeline-item wow fadeInRight"
-            data-wow-delay="0.5s"
-            data-wow-offset="50"
+            className='timeline-item wow fadeInRight'
+            data-wow-delay='0.5s'
+            data-wow-offset='50'
           >
-            <div className="timeline-info">
+            <div className='timeline-info'>
               <h4>Mar 2020 - Present</h4>
             </div>
-            <div className="timeline-marker"></div>
-            <div className="timeline-content">
-              <div className="timeline-title">
-                <h3>VMODEV VIETNAM</h3>
-                <h4 className="my-1">FRONT-END DEVELOPER</h4>
+            <div className='timeline-marker' />
+            <div className='timeline-content'>
+              <div className='timeline-title'>
+                <h3>VMO VIETNAM</h3>
+                <h4 className='my-1'>FRONT-END DEVELOPER</h4>
               </div>
-              <p>Combining the art of design with the art of programming.</p>
+              <p>- Combining the art of design with the art of programming.</p>
               <p>
-                - Translation of the UI/UX design wireframes to actual code that
-                will produce visual elements of the application.
+                + Translate the UI/UX design wireframes to actual code that
+                  will produce visual elements of the application.
+              </p>
+              <p>
+                + Work directly with customers.
+              </p>
+              <p>
+                + Project schedule Planning.
+              </p>
+                <p>
+                + Assign tasks to the team member and review the code
               </p>
             </div>
           </li>
           <li
-            className="timeline-item wow fadeInLeft"
-            data-wow-delay="0.5s"
-            data-wow-offset="50"
+            className='timeline-item wow fadeInLeft'
+            data-wow-delay='0.5s'
+            data-wow-offset='50'
           >
-            <div className="timeline-info">
+            <div className='timeline-info'>
               <h4>Dec 2019 - Mar 2020</h4>
             </div>
-            <div className="timeline-marker"></div>
-            <div className="timeline-content">
-              <div className="timeline-title">
+            <div className='timeline-marker' />
+            <div className='timeline-content'>
+              <div className='timeline-title'>
                 <h3>IMPL VIETNAM</h3>
-                <h4 className="my-1">FULL STACK WEB DEVELOPER</h4>
+                <h4 className='my-1'>FULL STACK WEB DEVELOPER</h4>
               </div>
-              <p>Building multi web applications for Japanese customer</p>
+              <p>- Build and maintained multi Japanese's web applications</p>
               <p>
-                - Building management using Laravel, Jquery, Mysql, Bootstrap
+                + Analyze the customer's requirement.
               </p>
-              <p>- Photo sharing using Electron, Laravel, VueJs, Postgresql</p>
+              <p>
+                + Provide solution and detail design.
+              </p>
             </div>
           </li>
           <li
-            className="timeline-item  wow fadeInRight"
-            data-wow-delay="0.5s"
-            data-wow-offset="50"
+            className='timeline-item  wow fadeInRight'
+            data-wow-delay='0.5s'
+            data-wow-offset='50'
           >
-            <div className="timeline-info">
+            <div className='timeline-info'>
               <h4>Jun 2018 - Dec 2019</h4>
             </div>
-            <div className="timeline-marker"></div>
-            <div className="timeline-content">
-              <div className="timeline-title">
+            <div className='timeline-marker' />
+            <div className='timeline-content'>
+              <div className='timeline-title'>
                 <h3>SUN* VIETNAM INC. (FRAMGIA)</h3>
-                <h4 className="my-1">FULL STACK WEB DEVELOPER</h4>
+                <h4 className='my-1'>FULL STACK WEB DEVELOPER</h4>
               </div>
               <p>
-                - Working as a full-stack developer mainly developing the Viblo
-                Platform.
+                - Work as a full-stack developer developing the Viblo platform.
               </p>
               <p>- Achievement:</p>
-              <p className="ml-4">
-                + Maintaining application with 60,000 daily active users.
+              <p className='ml-4'>
+                + Maintained application with 100,000 daily active users.
               </p>
-              <p className="ml-4">
-                + Building stunning and scalable web applications combining with
-                Backend using Laravel and Frontend using VueJs/NuxtJs or
-                ReactJs/NextJs.
+              <p className='ml-4'>
+                + Build multi stunning and scalable web applications using Laravel/NodeJs in the Backend and VueJs/NuxtJs or
+                  ReactJs/NextJs in the Frontend.
+                + Learn how to work with Docker.
+                + Provide solution and detail design.
               </p>
             </div>
           </li>
           <li
-            className="timeline-item  wow fadeInLeft"
-            data-wow-delay="0.5s"
-            data-wow-offset="50"
+            className='timeline-item  wow fadeInLeft'
+            data-wow-delay='0.5s'
+            data-wow-offset='50'
           >
-            <div className="timeline-info">
+            <div className='timeline-info'>
               <h4>Aug 2015 - Dec 2019</h4>
             </div>
-            <div className="timeline-marker"></div>
-            <div className="timeline-content">
-              <div className="timeline-title">
+            <div className='timeline-marker'></div>
+            <div className='timeline-content'>
+              <div className='timeline-title'>
                 <h3>POSTS AND TELECOMMUNICATIONS INSTITUTE OF TECHNOLOGY</h3>
-                <h4 className="my-1">INFORMATION TECHNOLOGY</h4>
+                <h4 className='my-1'>INFORMATION TECHNOLOGY</h4>
               </div>
               <p>- Graduated with Bachelor of Computer Science</p>
             </div>
--- src/components/Home/Skill/index.tsx
+++ src/components/Home/Skill/index.tsx
@@ -1,96 +1,96 @@
 import React, { FC } from 'react'
-import SvgLaravel from '../../Svg/SvgLaravel'
-import SvgNextJs from '../../Svg/SvgNextJs'
-import SvgReact from '../../Svg/SvgReact'
-import SvgJavascript from '../../Svg/SvgJavascript'
-import SvgTypescript from '../../Svg/SvgTypescript'
-import SvgVue from '../../Svg/SvgVue'
-import SvgHtml from '../../Svg/SvgHtml'
+import { Element } from 'react-scroll'
 import SvgBoostrap from '../../Svg/SvgBoostrap'
+import SvgCss from '../../Svg/SvgCss'
+import SvgDocker from '../../Svg/SvgDocker'
+import SvgHtml from '../../Svg/SvgHtml'
+import SvgJavascript from '../../Svg/SvgJavascript'
 import SvgJquery from '../../Svg/SvgJquery'
+import SvgLaravel from '../../Svg/SvgLaravel'
+import SvgMysql from '../../Svg/SvgMysql'
+import SvgNextJs from '../../Svg/SvgNextJs'
+import SvgNodejs from '../../Svg/SvgNodejs'
+import SvgNuxtJs from '../../Svg/SvgNuxtJs'
+import SvgReact from '../../Svg/SvgReact'
 import SvgRedux from '../../Svg/SvgRedux'
 import SvgReduxSaga from '../../Svg/SvgReduxSaga'
-import SvgNuxtJs from '../../Svg/SvgNuxtJs'
-import SvgNodejs from '../../Svg/SvgNodejs'
-import { Element } from 'react-scroll'
-import SvgDocker from '../../Svg/SvgDocker'
-import SvgMysql from '../../Svg/SvgMysql'
-import SvgCss from '../../Svg/SvgCss'
+import SvgTypescript from '../../Svg/SvgTypescript'
+import SvgVue from '../../Svg/SvgVue'
 import './index.scss'
 
 const Skill: FC = () => {
   return (
-    <Element className="bg-secondary skill font-mont" id="skill" name="skill">
-      <div className="container mx-auto py-24 px-4 sm:px-0">
-        <div className="section-title">
-          <h3 className="text-blue-500 ">My awesome Skills</h3>
+    <Element className='bg-secondary skill font-mont' id='skill' name='skill'>
+      <div className='container mx-auto py-24 px-4 sm:px-0'>
+        <div className='section-title'>
+          <h3 className='text-blue-500 '>My awesome Skills</h3>
           <h2>My skills</h2>
         </div>
-        <div className="grid grid-cols-3 lg:grid-cols-8 gap-4 mt-4 p-4">
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgLaravel className="lg:w-24 lg:h-24"></SvgLaravel>
-            <p className="mt-2">Laravel</p>
+        <div className='grid grid-cols-3 lg:grid-cols-8 gap-4 mt-4 p-4'>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgLaravel className='lg:w-24 lg:h-24'></SvgLaravel>
+            <p className='mt-2'>Laravel</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgNodejs className="lg:w-24 lg:h-24"></SvgNodejs>
-            <p className="mt-2">NodeJs</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgNodejs className='lg:w-24 lg:h-24'></SvgNodejs>
+            <p className='mt-2'>NodeJs</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgJavascript className="lg:w-24 lg:h-24"></SvgJavascript>
-            <p className="mt-2">Javascript</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgJavascript className='lg:w-24 lg:h-24'></SvgJavascript>
+            <p className='mt-2'>Javascript</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgTypescript className="lg:w-24 lg:h-24"></SvgTypescript>
-            <p className="mt-2">Typescript</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgTypescript className='lg:w-24 lg:h-24'></SvgTypescript>
+            <p className='mt-2'>Typescript</p>
           </div>
 
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgReact className="lg:w-24 lg:h-24"> </SvgReact>
-            <p className="mt-2">React</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgReact className='lg:w-24 lg:h-24'> </SvgReact>
+            <p className='mt-2'>React</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgVue className="lg:w-24 lg:h-24"></SvgVue>
-            <p className="mt-2">Vue</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgVue className='lg:w-24 lg:h-24'></SvgVue>
+            <p className='mt-2'>Vue</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgHtml className="lg:w-24 lg:h-24"></SvgHtml>
-            <p className="mt-2">HTML</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgHtml className='lg:w-24 lg:h-24'></SvgHtml>
+            <p className='mt-2'>HTML</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgCss className="lg:w-24 lg:h-24"></SvgCss>
-            <p className="mt-2">CSS</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgCss className='lg:w-24 lg:h-24'></SvgCss>
+            <p className='mt-2'>CSS</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgBoostrap className="lg:w-24 lg:h-24"></SvgBoostrap>
-            <p className="mt-2">Boostrap</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgBoostrap className='lg:w-24 lg:h-24'></SvgBoostrap>
+            <p className='mt-2'>Boostrap</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgJquery className="lg:w-24 lg:h-24"></SvgJquery>
-            <p className="mt-2">Jquery</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgJquery className='lg:w-24 lg:h-24'></SvgJquery>
+            <p className='mt-2'>Jquery</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgNextJs className="lg:w-24 lg:h-24"> </SvgNextJs>
-            <p className="mt-2">NextJs</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgNextJs className='lg:w-24 lg:h-24'> </SvgNextJs>
+            <p className='mt-2'>NextJs</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgRedux className="lg:w-24 lg:h-24"></SvgRedux>
-            <p className="mt-2">Redux</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgRedux className='lg:w-24 lg:h-24'></SvgRedux>
+            <p className='mt-2'>Redux</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgReduxSaga className="lg:w-24 lg:h-24"></SvgReduxSaga>
-            <p className="mt-2">Redux Saga</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgReduxSaga className='lg:w-24 lg:h-24'></SvgReduxSaga>
+            <p className='mt-2'>Redux Saga</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgNuxtJs className="lg:w-24 lg:h-24"></SvgNuxtJs>
-            <p className="mt-2">NuxtJs</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgNuxtJs className='lg:w-24 lg:h-24'></SvgNuxtJs>
+            <p className='mt-2'>NuxtJs</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgDocker className="lg:w-24 lg:h-24"></SvgDocker>
-            <p className="mt-2">Docker</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgDocker className='lg:w-24 lg:h-24'></SvgDocker>
+            <p className='mt-2'>Docker</p>
           </div>
-          <div className="card flex flex-col items-center justify-center p-2">
-            <SvgMysql className="lg:w-24 lg:h-24"></SvgMysql>
-            <p className="mt-2">MySql</p>
+          <div className='card flex flex-col items-center justify-center p-2'>
+            <SvgMysql className='lg:w-24 lg:h-24'></SvgMysql>
+            <p className='mt-2'>MySql</p>
           </div>
         </div>
       </div>
